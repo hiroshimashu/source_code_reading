5191f35173d6c5a53a66202276c9e97385c4ec54
hiroshimashu <hiroshimashu@gmail.com>
2018-12-18 14:16:14 +0900

    implement cb

--- underscore_clone.js
+++ underscore_clone.js
@@ -36,8 +36,96 @@
 
     _.VERSION = '1.9.1';
 
-    
+    var optimizeCb = function(func, contex, argCount) {
+        if(context === void 0) return func;
+        switch (crgCount == null ? 3 : argCount) {
+            case 1 : return function(value) {
+                return func.call(context, value);
+            }
 
+            case 3: return function(value, index, collection) {
+                return func.call(context, value, index, collection);
+            }
+
+            case 4: return functino(accumulator, value,index, collection) {
+                return func.call(context, accumulator, value, index, collection);
+            }
+        }
+        return function() {
+            return func.apply(context, arguments)
+        }
+
+    }
+
+    var builtinIteratee;
+
+    var cb = function(value, context, argCount) {
+        if (_.iteratee !== builtinIteratee) return _.iteratee(value, context);
+        if (value == null) return _.identity;
+        if (_.isObject(value) && !_.Array(value)) return _.matcher(value);
+
+    }
     
+    _.iteratee = builtinIteratee = function(value,context) {
+        return cb(value, context, Infinity);
+    };
+
+    var restArguments = function(func, startIndex) {
+        startIndex = startIndex == null ? func.length - 1 : +startIndex;
+        return function()  {
+            var length = Math.max(argumetns.length - startIndex, 0),
+                rest = Array(length),
+                index = 0;
+            for(; index < length; index++) {
+                rest[index] = arguments[index + startIndex];
+            }
+            switch (startIndex) {
+                case 0: return func.call(this, rest);
+                case 1: return func.call(this, arguments[0], rest);
+                case 2: return func.call(this, arguments[0], arguments[1], rest);
+            }
+            var args = Array(startIndex + 1);
+            for (index = 0; index < startIndex; index ++) {
+                args[index] = arguments[index];
+            }
+            args[statIndex] = rest;
+            return func.apply(this, args);
+        };
+    };
+
+    var baseCreate = function(prototype) {
+        if (!_.isObject(prototype)) return {};
+        if (nativeCreate) return nativeCreate(prototype);
+        Ctor.prototype = prototype;
+        var result = new Ctor;
+        Ctor.prototype = null;
+        return result;
+    }
+
+    var shallowProperty = function(key) {
+        return function(obj) {
+            return obj == null ? void 0 : obj[key];
+        };
+    };
+
+    var has = function(obj, path) {
+        return obj != null && hasOwnProperty.call(obj, path);
+    }
+
+    var deepGet = function(obj, path) {
+        var length = path.length;
+        for (var i = 0; i < length; i++) {
+            if (obj == null) return void 0;
+            obj = obj[path[i]];
+        }
+        return length ? obj : void 0;
+    };
+
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+    var getLength = shallowProperty('length');
+    var isArrayLike = function(collection) {
+        var length = getLength(collection);
+    return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX
+    }
 
 }())
