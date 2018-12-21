79ed8d5147ab618d86a2c7e1dde247dd82843274
hiroshimashu <hiroshimashu@gmail.com>
2018-12-21 17:24:18 +0900

    implement some and every method

--- underscore_clone.js
+++ underscore_clone.js
@@ -203,7 +203,29 @@
         return _.filter(obj, _.negate(cb(predicate)), context);
     };
 
-    
+    _.every = _.all = function(obj, predicate, context) {
+        predicate = cb(predicate, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+            length = (keys || obj).length;
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            if(!predicate(obj[currentKey], currentKey, obj)) return false;
+        }
+        return true;
+    }
+
+    _.some = _.any = function(obj, prediacate, context) {
+        predicate = cb(predicate, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+        length = (keys || obj).length;
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            if (prediacate(obj[currentKey], currentKey, obj)) return true;
+        }
+        return false;
+    }
+
+
 
 
 }())
