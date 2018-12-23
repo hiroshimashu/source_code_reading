6ebd253e0e3feeae8d91a7d23b4a4c45ffee1838
hiroshimashu <hiroshimashu@gmail.com>
2018-12-23 19:15:10 +0900

    implement where, findWhere, max method

--- underscore_clone.js
+++ underscore_clone.js
@@ -256,6 +256,35 @@
         return _.map(obj, _.property(key));
     };
 
+    _.where = function(obj, attrs) {
+        return _.filter(obj, _.matcher(attrs));
+    };
 
+    _.findWhere = function(obj, attrs) {
+        return _.find(obj, _.matcher(attrs));
+    }
 
+    _.max = function(obj, iteratee, context) {
+        var result = -Infinity, lastComputed = -Infinity,
+        value, computed;
+        if (iteratee == null || typeof iteratee == 'number' && typeof obj[0] != 'object' && obj != null) {
+            obj = isArrayLike(obj) ? obj : _.values(obj);
+            for (var i = 0, length = obj.length; i < length; i++) {
+                value = obj[i];
+                if(value != null && value > result) {
+                    result = value;
+                }
+            }
+        } else {
+            iteratee = cb(iteratee, context);
+            _.each(obj, function(v, index, list) {
+                computed = iteratee(v, index, list);
+                if (computed > lastComputed || computed === -Infinity && result === -Infinity) {
+                    result = v;
+                    lastComputed = computed;   
+                }
+            });
+        }
+        return result;
+    };
 }())
